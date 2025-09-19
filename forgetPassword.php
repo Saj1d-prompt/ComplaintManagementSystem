cc031b9beda9e967215e9d79e57bf7a95687e3dd
Saj1d-prompt <sajidulislam2225@gmail.com>
2025-09-19 14:48:32 +0600

    forget password front end

---
+++ forgetPassword.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Forget Password | Complaint Management System</title>
+    <link rel="stylesheet" href="forgetPassword.css">
+</head>
+<body>
+    <div class="login-container">
+        <label for="name">Email</label>
+        <input type="email" name="email" placeholder="Enter Your Email" required>
+        <label for="password">Old Password</label>
+        <input type="password" name="old_password" placeholder="Enter Your Old Password" required>
+        <label for="password">New Password</label>
+        <input type="password" name="new_password" placeholder="Enter Your New Password" required>
+        <button type="submit">Update</button>
+    </div>
+    
+</body>
+</html>
