ee59921fce321e81c7f5fd40cc66152ca4aedc60
Saj1d-prompt <sajidulislam2225@gmail.com>
2025-09-19 15:29:16 +0600

    forget password css applied

--- forgetPassword.php
+++ forgetPassword.php
@@ -8,12 +8,13 @@
 </head>
 <body>
     <div class="login-container">
+        <h2>Forget Password</h2>
         <label for="name">Email</label>
         <input type="email" name="email" placeholder="Enter Your Email" required>
         <label for="password">Old Password</label>
-        <input type="password" name="old_password" placeholder="Enter Your Old Password" required>
+        <input type="password" name="old_password" placeholder="Enter Old Password" required>
         <label for="password">New Password</label>
-        <input type="password" name="new_password" placeholder="Enter Your New Password" required>
+        <input type="password" name="new_password" placeholder="Enter New Password" required>
         <button type="submit">Update</button>
     </div>
     
